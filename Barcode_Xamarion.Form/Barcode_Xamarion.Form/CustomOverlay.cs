14b58a0c984017f6f844fccda5a239ee6c70a483
Wayne_C <q2a3z@hotmail.com>
2021-08-31 01:40:53 +0900

    Update UI Design

---
+++ Barcode_Xamarion.Form/Barcode_Xamarion.Form/CustomOverlay.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+
+namespace ZXing.Net.Mobile.Forms
+{ 
+    public class CustomOverlay : ZXingDefaultOverlay
+    {
+        public CustomOverlay()
+        {
+            foreach (var child in Children.OfType<BoxView>())
+            {
+                if (child == Children[2])
+                {
+                    child.BackgroundColor = Color.Transparent;
+                    Margin = new Thickness(0, 0);
+                }
+
+                else
+                {
+                    child.Opacity = 0.5;
+                }
+            }
+        }
+    }
+}
